861812ce4cb9cf4375fb0190e12e9a00b4b20dae
Akshat Nerella <91713635+akshatnerella@users.noreply.github.com>
2023-03-31 01:02:43 -0500

    more pins and resolved servos setup glitch

--- maincontroller.py
+++ maincontroller.py
@@ -1,7 +1,7 @@
 import pyfirmata #to interface w the arduino
 from tkinter import * #gui stuff
 import pygame #controller interfacing
-
+import time
 ###########SETUP################
 #Important Variables
 COM = 'COM5' #Update each time Arduino is connected
@@ -9,28 +9,48 @@ COM = 'COM5' #Update each time Arduino is connected
 #Declaring all the pins
 #Base
 global pin_base 
-#Arm hinge
+#Arm
 global pin_elbowR  
 global pin_elbowL
 #Hand
+global pin_thumbR
+global pin_thumbL
+global pin_index
+global pin_middle
+global pin_ring
+global pin_pinky
 
+#Boot
 board=pyfirmata.Arduino(COM)
 iter8 = pyfirmata.util.Iterator(board)
 iter8.start()
 
 #pin assignment
-pin_base   = board.get_pin('d:7:s')
+#Hand
+pin_thumbR  = board.get_pin('d:2:s')
+pin_thumbL  = board.get_pin('d:3:s')
+pin_index   = board.get_pin('d:4:s')
+pin_middle  = board.get_pin('d:5:s')
+pin_ring    = board.get_pin('d:6:s')
+pin_pinky   = board.get_pin('d:7:s')
+#Arm
 pin_elbowR = board.get_pin('d:8:s')
 pin_elbowL = board.get_pin('d:9:s')
+#Base
+pin_base   = board.get_pin('d:10:s')
 
-#Joystick part
+# Move servos to starting position
+pin_elbowR.write(90)
+pin_elbowL.write(90)
+
+#Joystick setup
 pygame.joystick.init()
 joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
 j = pygame.joystick.Joystick(0) #added name to joystick
 pygame.init()
 clock = pygame.time.Clock()
 
-################FUNCTIONS##############
+################ FUNCTIONS ##############
 def map_range(value, from_min, from_max, to_min, to_max):
     # Map a value from one range to another
     return (value - from_min) * (to_max - to_min) / (from_max - from_min) + to_min
@@ -42,17 +62,42 @@ def rotate_base(angle):
 
 def rotate_elbow(angle):
     # Map joystick input to servo angle range
-    mapped_angle = int(map_range(angle, -1, 1, 0, 180))
-    pin_elbowR.write(mapped_angle)
-    pin_elbowL.write(180 - mapped_angle)
+    angleR = int(map_range(angle, -1, 1, 0, 180))
+    angleL = int(map_range(angle, 1, -1, 0, 180))
+    pin_elbowR.write(angleR)
+    pin_elbowL.write(angleL)
 
-##############MAIN##################
+def hold():
+    var = 180 #fine tune this
+    pin_thumbR.write(var)
+    pin_thumbL.write(var)
+    pin_index.write(var)
+    pin_middle.write(var)
+    pin_ring.write(var)
+    pin_pinky.write(var)
+
+def release():
+    var = 0 # fine tune this
+    pin_thumbR.write(var)
+    pin_thumbL.write(var)
+    pin_index.write(var)
+    pin_middle.write(var)
+    pin_ring.write(var)
+    pin_pinky.write(var)
+
+def testfunc():
+    print("Success!")
+############## MAIN ##################
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             # Exit the program if the window is closed
             pygame.quit()
-            sys.exit()
+            Sys.exit()
+        #Buttons and Functions
+        if j.get_button(1): #A
+            testfunc()
+        
 
     # Get the current joystick position
     x_axis = j.get_axis(0)
@@ -67,7 +112,7 @@ while True:
     # Wait for a short time to avoid overwhelming the Arduino board
     pygame.time.wait(10)
 
-##########SCRATCH############
+########## MISC ############
 #Using Tkinter and PC
     # root = Tk()
     # root.geometry("1000x1000")
